25d027cad8d32a61c366b7b92beadcbbffba6a1d
GitSaranga <bsaranga@gmail.com>
2020-11-22 17:29:23 +0530

    Controller tests, continue from page - 288

--- CommandAPISolution/test/CommandAPI.Tests/CommandsControllerTests.cs
+++ CommandAPISolution/test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -3,26 +3,35 @@ using System.Collections.Generic;
 using AutoMapper;
 using CommandAPI.Controllers;
 using CommandAPI.Data;
+using CommandAPI.DTOs;
 using CommandAPI.Models;
 using CommandAPI.Profiles;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
 
 namespace CommandAPI.Tests
 {
-    public class CommandsControllerTests
+    public class CommandsControllerTests : IDisposable
     {
-        [Fact]
-        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty() {
-            // Arrange
-            var mockRepo = new Mock<ICommandAPIRepo>();
-            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
 
-            var realProfile = new CommandsProfile();
-            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
-            IMapper mapper = new Mapper(configuration);
-
-            var controller = new CommandsController(mockRepo.Object, mapper);
+        public void Dispose()
+        {
+            mockRepo = null;
+            realProfile = null;
+            configuration = null;
+            mapper = null;
         }
 
         private List<Command> GetCommands(int num)
@@ -38,5 +47,36 @@ namespace CommandAPI.Tests
             }
             return commands;
         }
+
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty() {
+
+            // Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            // Act
+            var result = controller.GetAllCommands();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource() {
+            
+            // Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            // Act
+            var result = controller.GetAllCommands();
+
+            // Assert
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+        
     }
 }
